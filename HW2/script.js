31967349c3c8e9c5f5a41b96e4249b161afaacb0
Jack <Jlawless97@gmail.com>
2019-01-26 10:23:09 -0700

    push

--- HW2/script.js
+++ HW2/script.js
@@ -7,3 +7,18 @@ console.log('Any Hots resturant back in Rochester, NY to get a super awesome Gar
 console.log('and Schooners Riverside Pub, where I grew up eating and working.');
 console.log('I like to munch on a good trail mix, pretzels, and ice cream if that counts.');
 console.log('Canteloupe no doubt. A perfectly ripe Mango, and watermelon');
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow>18){
+  greeting = 'Good evening';
+} else if(hourNow > 12) {
+  greeting = 'Good afternoon';
+} else if(hourNow > 0){
+  greeting = 'Good morning';
+}else {
+  greeting = 'Welcome';
+}
+document.write(greeting);
