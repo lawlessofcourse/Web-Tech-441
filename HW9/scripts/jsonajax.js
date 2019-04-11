e7579661ee8da6e3cf153fcda6e91808753ba526
Jack Lawless <31635829+lawlessofcourse@users.noreply.github.com>
2019-04-10 20:17:00 -0600

    update

--- HW9/scripts/jsonajax.js
+++ HW9/scripts/jsonajax.js
@@ -1,7 +1,29 @@
 var der;
-
+(function($) {
+  $.fn.blackbg = function() {
+    this.css("background-color", "black");
+    this.css("color", "white");
+    this.css("font-size", 24);
+    this.css("text-align", "center");
+    this.css("background-blend-mode", "overlay");
+    this.css("opacity", "0.75");
+    return this;
+  };
+}(jQuery));
+(function($) {
+  $.fn.bgImage = function() {
+    this.css("background-image", "url('./images/RickandMorty.jpg')");
+    this.css("background-position", "center");
+    this.css("background-size", "fit");
+    this.css("background-repeat", "repeat");
+    return this;
+  };
+}(jQuery));
 $(document).ready(function() {
   $("button").click(function() {
+    $("body").bgImage().fadeIn("fast");
+    $("#showInfo").blackbg().fadeIn("slow");
+    $("#btnSubmit").fadeOut("fast");
     $.getJSON("./data/shows.json", function(data) {
 
       objectCheck(data);
@@ -32,9 +54,9 @@ function objectCheck(data) {
     }
     if (key === 'number') {
       result += "Episode " + val + "<br>";
-      
+
     }
-    $("div").append(result);
+    $("#showInfo").append(result);
   });
 }
 
