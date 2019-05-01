1cfa4a775649758661f1959a2a719a4401d81768
Jack <Jlawless97@gmail.com>
2019-05-01 13:57:17 -0600

    ah ss push it

---
+++ Lawless_Boot/scripts/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+
+        });
+        $(document).ready(function () {
+            $(".btn").click(function () {
+                $(".navbar-default").toggle();
+            });
+
+        });
